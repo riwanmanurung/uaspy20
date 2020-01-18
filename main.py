7f598bd8638a4bf66f8e7bd37aceac4204b57560
riwanmanurung <riwanmanurung97@gmail.com>
2020-01-18 17:58:19 +0700

    commit to origin

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-class MainApp():
+from view.view_book import *
+from view.input_book import *
+from core.baseapp import BaseApp
+from core.search_helper import SearchHelper
+
+class MainApp(BaseApp):
     def __init__(self):
         self.books = []
+
+    def list_book(self):
+        ViewBook.list()
+
+    def add_book(self):
+        InputBook.input()
 
 
 if __name__ == "__main__":
